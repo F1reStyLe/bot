ab45059d7f91dc51ada17e5d2afa2dc5a7929c2f
F1reStyLe <iwgw@bk.ru>
2024-06-22 17:50:02 +0300

    basic config

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-bot = Bot(token='123')
+from config import Config
+
+bot = Bot(token=Config.token)
 dp = Dispatcher()
+
+@dp.message(CommandStart)
+async def cmd_start(message: Message):
+    await message.answer('Hello')
+
 
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    if Config.debug:
+        logging.basicConfig(level=logging.INFO)
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
