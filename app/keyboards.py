0c69869b215a3ad39c38241a3d61dd2deb8edaa4
F1reStyLe <iwgw@bk.ru>
2024-06-28 22:49:00 +0300

    added way for add routines

--- app/keyboards.py
+++ app/keyboards.py
@@ -3,3 +3,18 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 settings = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Календарь', callback_data="Calendar")]
 ])
+
+crud = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Добавить', callback_data="cal_add"),
+        InlineKeyboardButton(text='Удалить', callback_data="cal_del"),
+        InlineKeyboardButton(text='Удалить всё', callback_data="del_all"),
+    ]
+])
+
+yes_no = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Да', callback_data="yes"),
+        InlineKeyboardButton(text='Нет', callback_data="no"),
+    ]
+])
